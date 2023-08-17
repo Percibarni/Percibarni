47240072531b122b8680aa4dae012d70ce2eb1be
Percibarni <142522533+Percibarni@users.noreply.github.com>
2023-08-17 14:09:40 -0300

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,5 @@
-est- 👋 Hi, I’m @Percibarni
-- 👀 I’m interested in ...
-- 🌱 I’m currently learning ...
-- 💞️ I’m looking to collaborate on ...
-- 📫 How to reach me ...
+import pandas as pd
 
-<!---
-Percibarni/Percibarni is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+df = pd.read_excel("C:\Users\10891\Desktop\Macros\17082023.xlsx")
+
+print(df)
